22b2973025dcc30aa178104a05ed830d510b8953
Suhaas Teja <Suhaas@zestiot.io>
2022-09-03 21:29:53 +0530

    added github code link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h2>Tic-Tac-Toe</h2>
+        <h2>Tic-Tac-Toe <a href='https://github.com/suhaasteja/tic-tac-toe-react' target = "__blank" title='github link'>🔗</a></h2>
       </header>
       <Grid />
     </div>
